ca0dc892c8a7b27384e8cc4b97598d18e19ffad3
Turk <tuckerfiumara@gmail.com>
2019-12-05 21:31:23 -0700

    Prettied things up a bit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ class StickyNavigation {
 		$(window).resize(() => { this.onResize(); });
 	}
 	
-	onTabClick(event, element) {
+    onTabClick(event, element) {
 		event.preventDefault();
 		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
 		$('html, body').animate({ scrollTop: scrollTop }, 600);
