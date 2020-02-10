082cac6b81dfc9da0d5e7b21196d8eb44a88aa90
Turk <tuckerfiumara@gmail.com>
2020-02-09 22:25:10 -0700

    Added Skills and updated portfolio section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -79,5 +79,74 @@ new StickyNavigation();
 $('#submit').on("click", function() {
     event.preventDefault();
     document.getElementById("contact").reset();
-}) 
+});
+
+function openSkill(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
+
+
+  $(document).ready(function(){
+	var zindex = 10;
+	
+	$("div.port").click(function(e){
+  
+	  var isShowing = false;
+  
+	  if ($(this).hasClass("show")) {
+		isShowing = true
+	  }
+  
+	  if ($("div.cards").hasClass("showing")) {
+		// a card is already in view
+		$("div.port.show")
+		  .removeClass("show");
+  
+		if (isShowing) {
+		  // this card was showing - reset the grid
+		  $("div.cards")
+			.removeClass("showing");
+		} else {
+		  // this card isn't showing - get in with it
+		  $(this)
+			.css({zIndex: zindex})
+			.addClass("show");
+  
+		}
+  
+		zindex++;
+  
+	  } else {
+		// no cards in view
+		$("div.cards")
+		  .addClass("showing");
+		$(this)
+		  .css({zIndex:zindex})
+		  .addClass("show");
+  
+		zindex++;
+	  }
+	  
+	});
+  });
+
    
